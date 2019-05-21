f87cffabfb8b352fe5c695d8f3050dc6d03acf62
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2019-05-21 16:27:48 -0400

    Compare x.y.z semver

---
+++ src/main/kotlin/com/oliverspryn/library/Semver.kt
@@ -0,0 +1,50 @@
+package com.oliverspryn.library
+
+class Semver(version: String) {
+
+    private val major: Int
+    private val minor: Int
+    private val patch: Int
+
+    init {
+        val parts = version.split(".")
+
+        if (parts.size != 3) {
+            throw IllegalArgumentException("$version does not conform to semver conventions")
+        }
+
+        major = parts[0].toInt()
+        minor = parts[1].toInt()
+        patch = parts[2].toInt()
+    }
+
+    operator fun compareTo(semver: Semver): Int {
+        val theseParts = arrayOf(major, minor, patch)
+        val thoseParts = arrayOf(semver.major, semver.minor, semver.patch)
+
+        for (i in 0..2) {
+            if (theseParts[i] > thoseParts[i]) {
+                return 1
+            } else if (theseParts[i] < thoseParts[i]) {
+                return -1
+            }
+        }
+
+        return 0
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (other !is Semver) return false
+
+        val theseParts = arrayOf(major, minor, patch)
+        val thoseParts = arrayOf(other.major, other.minor, other.patch)
+
+        for (i in 0..2) {
+            if (theseParts[i] != thoseParts[i]) {
+                return false
+            }
+        }
+
+        return true
+    }
+}
